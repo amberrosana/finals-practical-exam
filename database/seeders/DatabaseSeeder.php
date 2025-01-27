c089b26decaaaf151f6ac340cb60937ee803fc0b
Your Name <you@example.com>
2025-01-27 20:46:28 +0800

    test commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,12 +5,24 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        // Create 10 random users
         $users = User::factory(10)->create();
+
+        $testUser = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345678'),  
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $testUser->id,  
+        ]);
 
         $users->each(function ($user) {
             Post::factory(5)->create([
@@ -18,9 +30,4 @@ class DatabaseSeeder extends Seeder
             ]);
         });
     }
-
-    // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
 }
